2256bd1207115a8a4c3e95b8da1084362f16821a
Oscar Saavedra <saavedraog@outlook.com>
2022-10-25 00:46:24 -0500

    estilo al initial a root. - Estilo layout-fixed. - En index.js se agrega el css al root antes de renderizar

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+let element = document.getElementById('root');
+element.setAttribute('class', 'layout-fixed');
+const root = ReactDOM.createRoot(element);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
